e0d5878a78a35e44e3574c6171c22e1cc8362ecd
aL1sia <104476750+aL1sia@users.noreply.github.com>
2025-03-21 14:14:49 +0800

    1

--- web/3.20/assets/js/tr.js
+++ web/3.20/assets/js/tr.js
@@ -1,2 +1,12 @@
+alert("温馨提示:部分内容可能过于耀眼,请佩戴护目镜观看");
 
 console.log("随便写的");
+
+function searchBut(){
+    alert("会但不多是这样的");
+
+}
+
+function openLink(){
+    window.open('https://www.baidu.com','_blank');
+}
